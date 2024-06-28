4d6aa0108fc67bf6c280d7d9b28c148a8eaafd4b
Federico Bustaffa <kiko.bustaffa@gmail.com>
2024-06-28 15:31:06 +0200

    premake

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "imgui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir)
+	objdir ("obj/" .. outputdir)
+
+	includedirs { ".", "../glfw/include" }
+
+	files {
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
